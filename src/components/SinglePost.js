a6e8914e0945438d091b8f5e1419b8771cd124f0
Felix <felix_reumann@gmx.de>
2022-06-02 20:47:38 +0200

    mini change

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -57,9 +57,7 @@ export default function SinglePost() {
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          ...
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
